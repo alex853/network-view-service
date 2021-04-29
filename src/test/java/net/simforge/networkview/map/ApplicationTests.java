6b37109406679e0d01484672e71bef4e17c396f3
alex853 <alexxxey.kornev@gmail.com>
2021-04-29 22:49:03 +0400

    Project imported

---
+++ src/test/java/net/simforge/networkview/map/ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.simforge.networkview.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
