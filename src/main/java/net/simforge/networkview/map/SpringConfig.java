088afda9910f5afe3bb75525b3d6b35c74cac468
alex853 <alexxxey.kornev@gmail.com>
2021-06-08 21:29:52 +0400

    support for 2 networks added - properties location fixed

--- src/main/java/net/simforge/networkview/map/SpringConfig.java
+++ src/main/java/net/simforge/networkview/map/SpringConfig.java
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,7 +12,6 @@ import javax.sql.DataSource;
 import java.util.HashMap;
 
 @Configuration
-@PropertySource({"classpath:application.properties"})
 @EnableScheduling
 public class SpringConfig {
 
